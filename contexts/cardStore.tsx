83c1c9c1fe02eb59e1f9a56c85420ef3582f8ae0
cococov <juank.yea@hotmail.com>
2021-07-19 12:58:38 -0400

    feat_ move fetch data to client

--- contexts/cardStore.tsx
+++ contexts/cardStore.tsx
@@ -1,5 +1,47 @@
 import { createContext, useState, useEffect } from 'react';
-import { replace } from 'rambda';
+import { replace, includes } from 'rambda';
+
+/**
+ * Requests
+ */
+const fetchData = async (cardName: string) => {
+  const rawResult = await fetch(`https://api.scryfall.com/cards/named?exact=${cardName}`);
+  const result = await rawResult.json();
+  const rawAllPrints = await fetch(result['prints_search_uri']);
+  const allPrints = await rawAllPrints.json();
+  const GARBAGE_EDITIONS = ['Intl. Collectors’ Edition', 'Collectors’ Edition', 'Legacy Championship', 'Summer Magic / Edgar'];
+
+  const print = allPrints['data'].reduce(
+    (accumulator: any, current: any) => {
+      if (current['digital']) return accumulator;
+      if (current['oversized']) return accumulator;
+      if (current['border_color'] === 'gold') return accumulator;
+      if (includes(current['set_name'], GARBAGE_EDITIONS)) return accumulator;
+      if (!current['prices']['usd']) return accumulator;
+      const currentPrice = parseFloat(current['prices']['usd']);
+      const accumulatedPrice = parseFloat(accumulator['prices']['usd']);
+      if (currentPrice >= accumulatedPrice) return accumulator;
+      if (current['multiverse_ids'].length === 0) return { ...current, multiverse_ids: accumulator['multiverse_ids'] }
+      return current;
+    },
+    result
+  );
+
+  return {
+    type: print['type_line'],
+    mana_cost: print['mana_cost'],
+    cmc: print['cmc'],
+    color_identity: print['color_identity'],
+    rarity: print['rarity'],
+    text: print['oracle_text'],
+    gathererId: print['multiverse_ids'][0],
+    averagePrice: print['prices']['usd'],
+    isReservedList: print['reserved'],
+    image_uris: print['image_uris'],
+    card_faces: print['card_faces'],
+  };
+};
+
 
 /**
  * Default Values
@@ -45,8 +87,7 @@ export const CardProvider: React.FC = ({ children }) => {
       setIsReservedList(DEFAULT_VALUES['isReservedList']);
 
       const cardName = replace(/\s/g, '%20', selectedCard);
-      const rawResult = await fetch(`/api/card?name=${cardName}`);
-      const result = await rawResult.json();
+      const result = await fetchData(cardName);
 
       if (!!result['card_faces'] && !!result['card_faces'][0]['image_uris']) {
         setCardImage(result['card_faces'][0]['image_uris']['large']);
