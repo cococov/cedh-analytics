6559a3aed052028be08e45ffe362e711e7ca2bdc
Juan Carlos Lamas Alfaro <juank.yea@hotmail.com>
2021-09-17 21:08:41 -0300

    feat: finish data endpoint

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -6,6 +6,11 @@ import {
   split,
   trim,
   map,
+  flatten,
+  mergeAll,
+  pipe,
+  isNil,
+  __
 } from 'ramda';
 
 type DbCommander = {
@@ -112,6 +117,7 @@ type MoxfieldCardEntry = {
 type MoxfieldData = {
   id: string,
   name: string,
+  url: string,
   description: string,
   format: string,
   visibility: string,
@@ -145,8 +151,45 @@ type MoxfieldData = {
   viewSettings: object,
 };
 
-type Data = {
-  decklistsData: MoxfieldData[],
+type CardData = {
+  occurrences: number,
+  cardName: string,
+  colorIdentity: string,
+  deckLinks: string[],
+  deckNames: string[],
+  manaCost: string,
+  prices: MoxfieldCardPrices,
+  reserved: boolean,
+  scrapName: string,
+  type: string,
+  typeLine: string,
+}
+
+type cardDataHash = {
+  [key: string]: CardData,
+}
+
+type Data = CardData[];
+
+const getType = (type: string): string => {
+  switch (type) {
+    case '1':
+      return 'Planeswalker';
+    case '2':
+      return 'Creature';
+    case '3':
+      return 'Sorcery';
+    case '4':
+      return 'Instant';
+    case '5':
+      return 'Artifact';
+    case '6':
+      return 'Enchantment';
+    case '7':
+      return 'Land';
+    default:
+      return 'Unknown';
+  }
 };
 
 const handler = async (
@@ -156,20 +199,59 @@ const handler = async (
   try {
     const rawLists = await fetch('https://raw.githubusercontent.com/AverageDragon/cEDH-Decklist-Database/master/_data/database.json');
     const lists: DbEntry[] = await rawLists.json();
-    const allDecklists: string[] = reduce(
+    const allCompetitiveDecklists: string[] = reduce(
       (accumulated: string[], current: DbEntry) => {
-        return [...accumulated, ...current['decklists'].map((decklist: DbDecklist): string => trim(decklist['link']))];
+        if (current.section !== 'COMPETITIVE') return accumulated;
+        return [...accumulated, ...current.decklists.map((decklist: DbDecklist): string => trim(decklist.link))];
       }, [], lists
     );
-    const filteredDecklists = filter((list: string) => includes('moxfield', list) && (split('/', list).length === 5), allDecklists);
+    const filteredDecklists = filter((list: string) => includes('moxfield', list) && (split('/', list).length >= 5), allCompetitiveDecklists);
     const deckListHashes = map((list: string) => split('/', list)[4], filteredDecklists);
     const delcklistsData: MoxfieldData[] = await Promise.all(map(async (hash: string) => {
       const rawData = await fetch(`https://api.moxfield.com/v2/decks/all/${hash}`);
       const data: MoxfieldData = await rawData.json();
-      return data;
+      return { ...data, url: `https://www.moxfield.com/decks/${hash}` };
     }, deckListHashes));
+    const cards: CardData[] = pipe(
+      map((data: MoxfieldData) => (
+        {
+          deck: { name: data.name, url: data.url },
+          cards: flatten(Object.values(mergeAll([data.mainboard, data.companions, data.commanders])))
+        })),
+      reduce((accumulated: cardDataHash, current): cardDataHash => {
+        const newValue: cardDataHash = reduce(
+          (accumulated2: cardDataHash, current2): cardDataHash => {
+            const hash: CardData = {
+              occurrences: 0,
+              cardName: current2.card.name,
+              colorIdentity: current2.card.color_identity.join(''),
+              deckLinks: [current.deck.url],
+              deckNames: [current.deck.name],
+              manaCost: current2.card.mana_cost,
+              prices: current2.card.prices,
+              reserved: current2.card.reserved,
+              scrapName: current2.card.name,
+              type: getType(current2.card.type),
+              typeLine: current2.card.type_line,
+            }
+            if (isNil(accumulated2[current2.card.name])) return { ...accumulated2, [current2.card.name]: hash };
+            return {
+              ...accumulated2,
+              [current2.card.name]: {
+                ...hash,
+                occurrences: ++accumulated2[current2.card.name].occurrences,
+                deckLinks: [...accumulated2[current2.card.name].deckLinks, current.deck.url],
+                deckNames: [...accumulated2[current2.card.name].deckNames, current.deck.name],
+              }
+            };
+          }, accumulated, current.cards
+        );
+        return newValue;
+      }, {}),
+      Object.values,
+    )(delcklistsData);
 
-    res.status(200).json({ decklistsData: delcklistsData });
+    res.status(200).json(cards);
   } catch (e: any) {
     res.status(500).json(e);
   }
