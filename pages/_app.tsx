1dbf8bba7e28023633889caca77ef0b5411da7fa
cococov <juank.yea@hotmail.com>
2021-11-23 13:16:00 -0300

    feat: implement hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    hotjar.initialize(2715865, 6);
+  }, []);
+
   return <Component {...pageProps} />
 }
 export default MyApp
