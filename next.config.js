d36a8c9c9c75c13acaf079ec7b2ed7028b357dc6
cococov <juank.yea@hotmail.com>
2022-05-06 02:08:21 -0400

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     minimumCacheTTL: 60 * 60 * 24, // 1 day
     domains: ['c1.scryfall.com'],
-},
+  },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
 }
