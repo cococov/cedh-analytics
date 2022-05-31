e4510725411abe16c3de219579d20907c9fea25b
cococov <juank.yea@hotmail.com>
2022-05-31 01:52:57 -0400

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { hotjar } from 'react-hotjar';
 import '../styles/globals.css';
 
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  const handleRouteChange = (url: string) => {
+    window.gtag('config', 'G-DQ9YFFQRG1', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     hotjar.initialize(2715865, 6);
   }, []);
