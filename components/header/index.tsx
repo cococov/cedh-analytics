ed6f7816aaa1a3910f94554197ec8d090b1ad96d
cococov <juank.yea@hotmail.com>
2022-06-13 00:25:08 -0400

    feat: improve og headers

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -9,6 +9,7 @@ const Header: React.FC<{
       <title>{!!title ? `${title} | ` : ''} cEDH Analytics</title>
       <meta name="description" content={`${!!description ? `${description} | ` : ''} cEDH Analytics is a website that analyzes and cross-references several EDH community's resources to give insights on the competitive metagame.`} />
       <meta property="og:image" content="https://www.cedh-analytics.com/images/carrot_compost_white.png" />
+      <meta property="og:image:secure_url" content="https://www.cedh-analytics.com/images/carrot_compost_white.png" />
       <meta property="og:site_name" content="cEDH Analytics" />
       <meta property="og:title" content={`${!!title ? `${title} | ` : ''} cEDH Analytics`} />
       <meta property="og:url" content="https://cedh-analytics.com/" />
@@ -16,6 +17,7 @@ const Header: React.FC<{
       <meta property="og:type" content="website" />
       <meta property="og:image:width" content="788" />
       <meta property="og:image:height" content="788" />
+      <meta property="og:updated_time" content="2022-06-13" />
       <meta itemProp="name" content={`${!!title ? `${title} | ` : ''} cEDH Analytics`} />
       <meta itemProp="url" content="https://cedh-analytics.com/" />
       <meta itemProp="description" content={`${!!description ? `${description} | ` : ''} cEDH Analytics is a website that analyzes and cross-references several EDH community's resources to give insights on the competitive metagame.`} />
