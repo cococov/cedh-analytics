e41b563febd6dfdb477d6ea1a2e7755c15ac03ce
cococov <contact@juanlamas.dev>
2023-03-15 23:38:21 -0300

    "chore: update tournament {TOURNAMENT_ID}"

--- scripts/utils/logs.py
+++ scripts/utils/logs.py
@@ -8,4 +8,4 @@ def end_log_block(message):
   print('\033[K', message, ' \033[92mDone!\033[0m', sep='')
 
 def success_log(message):
-  print(rf'\033[K\033[92m{message}\033[0m')
+  print('\033[K\033[92m', message, '\033[0m', sep='')
