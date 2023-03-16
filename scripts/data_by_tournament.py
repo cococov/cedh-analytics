1704b47426ac8fde912a960d93e5f32a93e4afe3
cococov <contact@juanlamas.dev>
2023-03-15 23:37:35 -0300

    "chore: update tournament {TOURNAMENT_ID}"

--- scripts/data_by_tournament.py
+++ scripts/data_by_tournament.py
@@ -28,17 +28,17 @@ begin_log_block('Deleting csv directory content')
 
 clear_csv_directory()
 
-begin_log_block('csv directory content deleted')
+end_log_block('csv directory content deleted')
 begin_log_block('Geting all printing')
 
 download_file(ALL_PRINTS_URL, './csv')
 
-begin_log_block('Geting all printing')
+end_log_block('Geting all printing')
 begin_log_block('Unzip all printing')
 
 unzip_file('./csv/AllPrintingsCSVFiles.zip', './csv')
 
-begin_log_block('Unzip all printing')
+end_log_block('Unzip all printing')
 begin_log_block('Processing all printing')
 
 cards_csv = get_cards_csv()
@@ -46,30 +46,30 @@ sets_csv = get_sets_csv(VALID_TYPE_SETS, INVALID_SETS)
 get_last_set_for_card = build_get_last_set_for_card(cards_csv, sets_csv)
 has_multiple_printings = build_has_multiple_printings(cards_csv, sets_csv)
 
-begin_log_block('Processing all printing')
+end_log_block('Processing all printing')
 begin_log_block('Getting decklists')
 
 lists = get_decklists_from_bookmark(TOURNAMENT_DECKLISTS_BOOKMARK_ID)
 
-begin_log_block('Getting decklists')
+end_log_block('Getting decklists')
 begin_log_block('Processing hashes')
 
 all_competitive_deck_hashes = get_decklist_hashes_from_bookmark(lists)
 data.moxfield.VALID_DECKS = len(all_competitive_deck_hashes)
 home_overview['decks'] = data.moxfield.VALID_DECKS
 
-begin_log_block('Procesing hashes')
+end_log_block('Procesing hashes')
 begin_log_block('Getting decklists data')
 
 decklists_data = get_decklists_data_from_hashes(all_competitive_deck_hashes)
 
-begin_log_block('Getting decklists data')
+end_log_block('Getting decklists data')
 begin_log_block('Pre-Processing decklists data')
 
 mapped_decklists_data = get_decklists_data(decklists_data)
 reduced_data = process_cards(reduce_decks_to_cards(mapped_decklists_data, has_multiple_printings, get_last_set_for_card))
 
-begin_log_block('Pre-Processing decklists data')
+end_log_block('Pre-Processing decklists data')
 begin_log_block('Processing decklists data')
 
 home_overview['cards'] = cards_number(reduced_data)
@@ -79,23 +79,23 @@ home_overview['pet'] = pet_cards_number(reduced_data)
 home_overview['last_set'] = LAST_SET[0]
 home_overview['last_set_top_10'] = last_set_top_10(reduced_data, LAST_SET)
 
-begin_log_block('Processing decklists data')
+end_log_block('Processing decklists data')
 begin_log_block('Saving new file')
 
 create_dir(FOLDER_PATH)
 create_file(DIRNAME, FILE_PATH, reduced_data)
 
-begin_log_block('New file saved')
+end_log_block('New file saved')
 begin_log_block('Updating home overview')
 
 create_file(DIRNAME, rf'public/data/tournaments/{TOURNAMENT_ID}/home_overview.json', home_overview)
 
-begin_log_block('Home overview saved')
+end_log_block('Home overview saved')
 begin_log_block('Cleaning')
 
 clear_csv_directory()
 
-begin_log_block('Cleaning')
+end_log_block('Cleaning')
 time.sleep(1)
 begin_log_block('Uploading changes')
 
--- scripts/utils/logs.py
+++ scripts/utils/logs.py
@@ -5,7 +5,7 @@ def begin_log_block(message):
   print(f'{message}...', end='\r')
 
 def end_log_block(message):
-  print(fr'\033[K{message} \033[92mDone!\033[0m')
+  print('\033[K', message, ' \033[92mDone!\033[0m', sep='')
 
 def success_log(message):
   print(rf'\033[K\033[92m{message}\033[0m')
