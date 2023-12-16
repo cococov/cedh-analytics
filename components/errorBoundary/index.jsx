3d687e4c458be343c6bde4de4295969462c5ec8d
cococov <contact@juanlamas.dev>
2023-12-15 23:39:00 -0300

    feat: caputre suspense errors in error boundary

---
+++ components/errorBoundary/index.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static defaultProps = {
+    fallback: <p>Something went wrong.</p>,
+  };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  };
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  };
+}
+
+export default ErrorBoundary;
