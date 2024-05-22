34398a4c494a8f6f3dfae7d5f0250deb3a37ac4e
cococov <contact@juanlamas.dev>
2024-05-22 01:22:11 -0400

    chore: add copyright to components

--- components/cardsTable/index.tsx
+++ components/cardsTable/index.tsx
@@ -1,3 +1,26 @@
+/**
+ *  cEDH Analytics - A website that analyzes and cross-references several
+ *  EDH (Magic: The Gathering format) community's resources to give insights
+ *  on the competitive metagame.
+ *  Copyright (C) 2021-present CoCoCov
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Original Repo: https://github.com/cococov/cedh-analytics
+ *  https://www.cedh-analytics.com/
+ */
+
 "use client";
 
 import { useState, useEffect, useCallback, useContext, useRef } from 'react';
