2e0a98037f022f4fbef65ad9d2821c3087e6e2b6
cococov <contact@juanlamas.dev>
2024-06-22 23:56:18 -0400

    feat: add patreon button

--- components/patreonButton/index.tsx
+++ components/patreonButton/index.tsx
@@ -2,7 +2,7 @@
  *  cEDH Analytics - A website that analyzes and cross-references several
  *  EDH (Magic: The Gathering format) community's resources to give insights
  *  on the competitive metagame.
- *  Copyright (C) 2023-present CoCoCov
+ *  Copyright (C) 2024-present CoCoCov
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -26,13 +26,14 @@ import Image from 'next/image';
 /* Vendor */
 import { Tooltip } from "@nextui-org/tooltip";
 /* Static */
-import styles from './kofiButton.module.css'
+import patreonLogo from '@/public/images/PATREON_SYMBOL_1_BLACK_RGB.png';
+import styles from './patreonButton.module.css'
 
 export default function KofiButton() {
   return (
-    <Tooltip content="Ko-Fi">
-      <Link href="https://ko-fi.com/carrotcompost" target="_blank" className={styles.kofiButton}>
-        <Image src="https://storage.ko-fi.com/cdn/nav-logo-stroke.png" alt="Ko-fi logo" width={32} height={22} />
+    <Tooltip content="Patreon">
+      <Link href="https://patreon.com/cEDHAnalytics" target="_blank" className={styles.patreonButton}>
+        <Image src={patreonLogo} alt="Patreon logo" width={20} height={22} />
         Support
       </Link>
     </Tooltip>
