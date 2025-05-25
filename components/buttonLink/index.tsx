b4eb5ec7d5501f97fc05b04fb0b86e1788428025
cococov <jlamas@buk.cl>
2025-05-25 03:49:24 -0400

    fix: type error

--- components/buttonLink/index.tsx
+++ components/buttonLink/index.tsx
@@ -21,20 +21,20 @@
  *  https://www.cedh-analytics.com/
  */
 
-import { forwardRef, Ref } from 'react';
+import { forwardRef, ForwardedRef } from 'react';
 import Link, { LinkProps } from 'next/link';
 import { Button } from '@/components/vendor/materialUi';
 
-type LinkRef = HTMLAnchorElement | HTMLButtonElement
-type NextLinkProps = (Omit<any, 'href'> &
-  Pick<LinkProps, 'href' | 'as' | 'prefetch' | 'locale'>) | any;
+type ButtonProps = React.ComponentPropsWithoutRef<typeof Button>;
+type NextLinkProps = Omit<ButtonProps, 'href'> & 
+  Pick<LinkProps, 'href' | 'as' | 'prefetch' | 'locale'>;
 
-function NextLink({ href, as, prefetch, locale, ...props }: LinkProps, ref: Ref<LinkRef>) {
+function NextLink({ href, as, prefetch, locale, ...props }: NextLinkProps, ref: ForwardedRef<HTMLButtonElement>) {
   return (
     <Link href={href} as={as} prefetch={prefetch} locale={locale} passHref>
-      <Button ref={ref as any} {...props as any} />
+      <Button ref={ref} {...props} />
     </Link>
   );
 };
 
-export default forwardRef<LinkRef, NextLinkProps>(NextLink);
+export default forwardRef<HTMLButtonElement, NextLinkProps>(NextLink);
