ed0b34d4e6eaf67002938cb815b358531ff5c5a1
cococov <jlamas@buk.cl>
2025-05-29 13:04:02 -0400

    fix: remove redundant quotes in git commit message command

--- scripts/utils/git.py
+++ scripts/utils/git.py
@@ -31,7 +31,7 @@ def add_all():
   check_call(['git', 'add', '.'], stdout=DEVNULL, stderr=STDOUT)
 
 def commit(message):
-  check_call(['git', 'commit', '-m', f'"{message}"'], stdout=DEVNULL, stderr=STDOUT)
+  check_call(['git', 'commit', '-m', message], stdout=DEVNULL, stderr=STDOUT)
 
 def push():
   check_call(['git', 'push'], stdout=DEVNULL, stderr=STDOUT)
